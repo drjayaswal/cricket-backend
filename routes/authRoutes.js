ffe44d2d67ed18e25d8110f292907b65704a0c77
Dhruv Ratan Jayaswal <mrdhruv.professional@gmail.com>
2025-05-01 19:28:05 +0530

    more fixes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -106,7 +106,7 @@ router.post("/login", async (req, res) => {
       return res.status(400).json({ message: "User not found" });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
