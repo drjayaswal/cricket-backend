82e5bfee3b030525c405e6cd54e6c0835f280acd
Dhruv Ratan Jayaswal <mrdhruv.professional@gmail.com>
2025-05-12 14:01:47 +0530

    chore: testing

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -9,13 +9,13 @@ import { Cashfree } from "cashfree-pg";
 
 const router = express.Router();
 
-const ID = process.env.TCASHFREE_ID;
-const SECRET = process.env.TCASHFREE_SECRET;
+const ID = process.env.CASHFREE_ID;
+const SECRET = process.env.CASHFREE_SECRET;
 const BACK = process.env.BACKEND_URL;
 const FRONT = process.env.FRONTEND_URL;
 Cashfree.XClientId = ID;
 Cashfree.XClientSecret = SECRET;
-Cashfree.XEnvironment = "SANDBOX";
+Cashfree.XEnvironment = "PRODUCTION";
 
 // Helper to delete all transactions for a user
 const deleteAllTransactions = async (mobile) => {
@@ -47,8 +47,8 @@ router.post("/create-order", authMiddleware, async (req, res) => {
       return res.status(404).json({ error: "User not found" });
     }
 
-    const orderId = `order_${Date.now()}`;
-    const transactionId = `txn_${uuidv4()}`;
+    const orderId = `ORDER_${Date.now()}`;
+    const transactionId = `TXN_${uuidv4()}`;
 
     user.transactions.push({
       TID: transactionId,
@@ -69,11 +69,13 @@ router.post("/create-order", authMiddleware, async (req, res) => {
         customer_phone: user.mobile,
       },
       order_meta: {
-        return_url: `${FRONT}/payment/orders/${orderId}`,
+        return_url: `${FRONT
+        .replace("http","https")
+        }/payment/orders/${orderId}`,
       },
     };
 
-    const response = await fetch("https://sandbox.cashfree.com/pg/orders", {
+    const response = await fetch("https://api.cashfree.com/pg/orders", {
       method: "POST",
       headers: {
         "x-api-version": "2025-01-01",
@@ -150,7 +152,7 @@ router.get("/check-order/:order_id", authMiddleware, async (req, res) => {
     }
 
     const response = await fetch(
-      `https://sandbox.cashfree.com/pg/orders/${order_id}`,
+      `https://api.cashfree.com/pg/orders/${order_id}`,
       {
         method: "GET",
         headers: {
@@ -191,6 +193,92 @@ router.get("/check-order/:order_id", authMiddleware, async (req, res) => {
   } catch (error) {
     console.error("Error checking order status:", error);
     res.status(500).json({ error: "Server error" });
+  }
+});
+
+
+// create a payment link generate route
+router.post("/create-link", authMiddleware, async (req, res) => {
+  try {
+    if (!req.user || !req.user.userId) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: No user data in token" });
+    }
+
+    const userId = req.user.userId;
+    const { amount } = req.body;
+
+    if (!amount || isNaN(amount) || Number(amount) <= 0) {
+      return res.status(400).json({ error: "Invalid amount" });
+    }
+
+    const user = await User.findOne({ _id: userId });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    await user.save();
+    
+    const orderId = `ORDER_${Date.now()}`;
+    const transactionId = `TXN_${uuidv4()}`;
+    const linkId = `LINK_${uuidv4()}`;
+    
+    user.transactions.push({
+      TID: transactionId,
+      OID: orderId,
+      amount: amount,
+      status: "PENDING",
+    });
+
+
+    const orderRequest = {
+      "link_id":linkId,
+      "link_amount":amount,
+      "link_currency":"INR",
+      "link_purpose":"CRICKSTOCK11 Funds",
+      "customer_details":{
+        "customer_phone":user.mobile,
+        "customer_name": user.name
+      },
+      "link_notify":{
+        "send_sms":true,
+        "send_email":true
+      }
+    }
+
+    const response = await fetch("https://api.cashfree.com/pg/links", {
+      method: "POST",
+      headers: {
+        "x-api-version": "2025-01-01",
+        "x-client-id": ID,
+        "x-client-secret": SECRET,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(orderRequest),
+    });
+
+    const result = await response.json();
+
+    if (result.order_status === "ACTIVE") {
+      res.status(200).json({
+        message: "Order created successfully",
+        orderDetails: {
+          orderId: result.order_id,
+          orderAmount: result.order_amount,
+          createdAt: result.created_at,
+          paymentSessionId: result.payment_session_id,
+        },
+      });
+    } else {
+      res.status(400).json({
+        error: "Failed to create order",
+        details: result.message || "Unknown error occurred.",
+      });
+    }
+  }catch(e){
+    console.log(e)
   }
 });
 
